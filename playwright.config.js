18db18aa1181d87761002c96f9bedeb27b8a46bd
f.weznerowicz <f.weznerowicz@sportradar.com>
2024-12-08 02:10:01 +0100

    added trace to PASS tests

--- playwright.config.js
+++ playwright.config.js
@@ -36,17 +36,26 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        trace: "on",
+        ...devices['Desktop Chrome']
+      },
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: {
+        trace: "on",
+        ...devices['Desktop Firefox']
+      },
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: {
+        trace: "on",
+        ...devices['Desktop Safari']
+      },
     },
 
     /* Test against mobile viewports. */
